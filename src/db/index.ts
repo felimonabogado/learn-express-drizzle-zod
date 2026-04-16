a268edd81f217155298baaba581a0f11593bd597
Felimon Abogado <felimonabogado@gmail.com>
2026-04-16 08:16:59 +0800

    Refactor: dotenv added, database credentials was now on env

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,13 @@
 import mysql from "mysql2/promise";
 import { drizzle } from "drizzle-orm/mysql2";
+import "dotenv/config";
 
 const pool = mysql.createPool({
-    host: "127.0.0.1",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "express_db",
+    host: process.env.DB_HOST || "127.0.0.1",
+    port: parseInt(process.env.DB_PORT || "3306"),
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "root",
+    database: process.env.DB_NAME || "express_db",
 });
 
 export const db = drizzle(pool);
